5c3a29e5fd5e62dd61aba2709b5391a513283c1d
Kocherha Roman <f.your.d.2017@gmail.com>
2024-12-03 13:32:25 +0200

    fuxed hover links adeed media games srction

--- components/sections/customHome/GoodsAnimation.js
+++ components/sections/customHome/GoodsAnimation.js
@@ -5,10 +5,8 @@ const GoodsAnimation = () => {
   return (
     <section className="section-box wow animate__animated animate__fadeIn box-logos-2 custom-logos">
       <div className='box-logos-custom'>
-        <div className="container">
-            <div className="carouselTickerLogos2 carouselTicker_vertical" id="slide-logos">
-                <LogoTicker2 />
-            </div>
+        <div className="carouselTickerLogos2 carouselTicker_vertical" id="slide-logos">
+          <LogoTicker2 />
         </div>
       </div>
     </section>
