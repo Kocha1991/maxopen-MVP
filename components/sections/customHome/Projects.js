461854b9016dd60f591bf7a5d04b3a5f68e9f7ae
Kocherha Roman <f.your.d.2017@gmail.com>
2024-12-06 17:30:32 +0200

    added portfolio projects to admin

--- components/sections/customHome/Projects.js
+++ components/sections/customHome/Projects.js
@@ -1,49 +1,60 @@
-"use client";
-import React, { useState } from "react";
+'use client';
+import React, { useState, useEffect } from "react";
 import { ProjectCard } from '@/components/elements/ProjectCard';
 import { BlogTitle } from '@/components/blog/BlogTitle';
 import Link from 'next/link';
 
 export const Projects = () => {
+  const [projects, setProjects] = useState([]);
+
+  // Отримання даних з API з авторизацією
+  useEffect(() => {
+    fetch('https://api.maxopen.com.ua/api/0b75148ea08740bd8c78fc4077500b5d/cases', {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Bearer c8TUpsSJoXrGQLD0laAtVwYOgJdGtEPm72xrA2SP',
+      }
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Отримуємо мову браузера
+        const language = navigator.language || navigator.userLanguage;
+        const lang = (language.startsWith('uk') || language.startsWith('ru')) ? 'uk' : 'en';
+        // Фільтруємо проекти по мові
+        const filteredProjects = data.filter(project => project.locale === lang);
+        // Перевірка, чи є дані масивом
+        if (Array.isArray(filteredProjects)) {
+          setProjects(filteredProjects);
+        } else {
+          console.error('Дані не є масивом:', filteredProjects);
+        }
+      })
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
+
   return (
-    <div className='maxOpen-projects' id="projects">
-      <div className='container'>
+    <div className="maxOpen-projects" id="projects">
+      <div className="container">
         <BlogTitle 
-          textOnBg='Our Portfolio'
-          title='Explore our projects to witness our expertise in action'
-          descr='Our portfolio showcases successful solutions crafted for businesses of all sizes. We help clients grow, enhance efficiency, and reach new heights. Review our work and see what we can do for you.'
+          textOnBg="Our Portfolio"
+          title="Explore our projects to witness our expertise in action"
+          descr="Our portfolio showcases successful solutions crafted for businesses of all sizes. We help clients grow, enhance efficiency, and reach new heights. Review our work and see what we can do for you."
         />
         <div className="row mt-65">
-            <ProjectCard 
-              img='/assets/imgs/template/project1.jpg'
-              title="Med Expert"
-              descr="Purus dui eget sollicitudin curae leo proin platea cras, morbi torquent massa"
-            />
-            <ProjectCard 
-              img='/assets/imgs/template/project2.jpg'
-              title="Impero barbers"
-              descr="Purus dui eget sollicitudin curae leo proin platea cras, morbi torquent massa"
-            />
-            <ProjectCard 
-              img='/assets/imgs/template/project3.jpg'
-              title="Nova mobile"
-              descr="Purus dui eget sollicitudin curae leo proin platea cras, morbi torquent massa"
-            />
-            <ProjectCard 
-              img='/assets/imgs/template/project4.jpg'
-              title="Expert"
-              descr="Purus dui eget sollicitudin curae leo proin platea cras, morbi torquent massa"
-            />
-            <ProjectCard 
-              img='/assets/imgs/template/project5.jpg'
-              title="Carpaccio"
-              descr="Purus dui eget sollicitudin curae leo proin platea cras, morbi torquent massa"
-            />
-            <ProjectCard 
-              img='/assets/imgs/template/project6.jpg'
-              title="Chromatelle"
-              descr="Purus dui eget sollicitudin curae leo proin platea cras, morbi torquent massa"
-            />
+          {/* Перевірка наявності проектів та чи це масив */}
+          {Array.isArray(projects) && projects.length > 0 ? (
+            projects.map((project) => (
+              <ProjectCard 
+                key={project.id} 
+                img={project['image-of-project'].thumb}
+                title={project['name-of-project']}
+                descr={project['short-description']}
+                link={project['link-to-project']}
+              />
+            ))
+          ) : (
+            <p>Немає доступних проектів</p>
+          )}
         </div>
         <Link className="btn btn-brand-4-medium hover-up" href="#">
           <span>Load more</span>
