d99486cdddc7fe7db1d49113e923c32fa561a2da
Kocherha Roman <f.your.d.2017@gmail.com>
2024-12-07 12:59:05 +0200

    added technology animation

--- components/elements/LogoTicker.js
+++ components/elements/LogoTicker.js
@@ -9,34 +9,58 @@ export default function LogoTicker() {
                     pauseOnHover={true}
                     direction="left" className="carouselTicker__list list-logos">
                     <li className="carouselTicker__item">
-                        <div className="item-logo"><img src="/assets/imgs/page/homepage3/logo1.png" alt="Nivia" /></div>
+                        <div className="item-logo"><img src="assets/imgs/template/Maxopen-technology/C.svg" alt="Nivia" /></div>
                     </li>
                     <li className="carouselTicker__item">
-                        <div className="item-logo"><img src="/assets/imgs/page/homepage3/logo2.png" alt="Nivia" /></div>
+                        <div className="item-logo"><img src="assets/imgs/template/Maxopen-technology/css.svg" alt="Nivia" /></div>
                     </li>
                     <li className="carouselTicker__item">
-                        <div className="item-logo"><img src="/assets/imgs/page/homepage3/logo3.png" alt="Nivia" /></div>
+                        <div className="item-logo"><img src="assets/imgs/template/Maxopen-technology/golang.svg" alt="Nivia" /></div>
                     </li>
                     <li className="carouselTicker__item">
-                        <div className="item-logo"><img src="/assets/imgs/page/homepage3/logo4.png" alt="Nivia" /></div>
+                        <div className="item-logo"><img src="assets/imgs/template/Maxopen-technology/Html.svg" alt="Nivia" /></div>
                     </li>
                     <li className="carouselTicker__item">
-                        <div className="item-logo"><img src="/assets/imgs/page/homepage3/logo5.png" alt="Nivia" /></div>
+                        <div className="item-logo"><img src="assets/imgs/template/Maxopen-technology/java.svg" alt="Nivia" /></div>
                     </li>
                     <li className="carouselTicker__item">
-                        <div className="item-logo"><img src="/assets/imgs/page/homepage3/logo6.png" alt="Nivia" /></div>
+                        <div className="item-logo"><img src="assets/imgs/template/Maxopen-technology/JS.svg" alt="Nivia" /></div>
                     </li>
                     <li className="carouselTicker__item">
-                        <div className="item-logo"><img src="/assets/imgs/page/homepage3/logo7.png" alt="Nivia" /></div>
+                        <div className="item-logo"><img src="assets/imgs/template/Maxopen-technology/kotlin.svg" alt="Nivia" /></div>
                     </li>
                     <li className="carouselTicker__item">
-                        <div className="item-logo"><img src="/assets/imgs/page/homepage3/logo8.png" alt="Nivia" /></div>
+                        <div className="item-logo"><img src="assets/imgs/template/Maxopen-technology/laravel.svg" alt="Nivia" /></div>
                     </li>
                     <li className="carouselTicker__item">
-                        <div className="item-logo"><img src="/assets/imgs/page/homepage3/logo9.png" alt="Nivia" /></div>
+                        <div className="item-logo"><img src="assets/imgs/template/Maxopen-technology/node,js.svg" /></div>
                     </li>
                     <li className="carouselTicker__item">
-                        <div className="item-logo"><img src="/assets/imgs/page/homepage3/logo1.png" alt="Nivia" /></div>
+                        <div className="item-logo"><img src="assets/imgs/template/Maxopen-technology/php.svg" alt="Nivia" /></div>
+                    </li>
+                    <li className="carouselTicker__item">
+                        <div className="item-logo"><img src="assets/imgs/template/Maxopen-technology/pyhton.svg" alt="Nivia" /></div>
+                    </li>
+                    <li className="carouselTicker__item">
+                        <div className="item-logo"><img src="assets/imgs/template/Maxopen-technology/react native.svg" alt="Nivia" /></div>
+                    </li>
+                    <li className="carouselTicker__item">
+                        <div className="item-logo"><img src="assets/imgs/template//Maxopen-technology/redux.svg" alt="Nivia" /></div>
+                    </li>
+                    <li className="carouselTicker__item">
+                        <div className="item-logo"><img src="assets/imgs/template//Maxopen-technology/unity.svg" alt="Nivia" /></div>
+                    </li>
+                    <li className="carouselTicker__item">
+                        <div className="item-logo"><img src="assets/imgs/template//Maxopen-technology/swift.svg" alt="Nivia" /></div>
+                    </li>
+                    <li className="carouselTicker__item">
+                        <div className="item-logo"><img src="assets/imgs/template//Maxopen-technology/unity.svg" alt="Nivia" /></div>
+                    </li>
+                    <li className="carouselTicker__item">
+                        <div className="item-logo"><img src="assets/imgs/template//Maxopen-technology/unreal.svg" alt="Nivia" /></div>
+                    </li>
+                    <li className="carouselTicker__item">
+                        <div className="item-logo"><img src="assets/imgs/template//Maxopen-technology/unty.svg" alt="Nivia" /></div>
                     </li>
                 </Marquee>
         </>
