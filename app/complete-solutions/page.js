68bdcfbd84fb897e385988785d51163227bc1845
Kocherha Roman <f.your.d.2017@gmail.com>
2025-01-02 16:05:18 +0200

    added translation solutions page

--- app/complete-solutions/page.js
+++ app/complete-solutions/page.js
@@ -1,8 +1,11 @@
+'use client';
 import { SolutionCard } from '@/components/elements/SolutionCard';
 import { SolutionCard2 } from '@/components/elements/SolutionCard2';
 import Layout from '@/components/layout/Layout';
-import Link from 'next/link';
 import { ModalProvider } from '@/components/customHooks/useModal';
+import { useTranslation } from 'react-i18next';
+import { useModal } from '@/components/customHooks/useModal';
+import ModalManager from '@/components/elements/ModalManager';
 
 export default function CompleteSolutions() {
   return (
@@ -13,6 +16,9 @@ export default function CompleteSolutions() {
 }
 
 function CompleteSolutionsContent() {
+  const { openModal, isOpen, modalType, modalData, closeModal } = useModal();  // Ось тут ви отримуєте значення з useModal хука
+  const { t } = useTranslation();
+
   return (
     <div className='complete-solutions'>
       <Layout useCustomHeader={true} footerStyle={'customFooter'} logoWhite>
@@ -20,17 +26,20 @@ function CompleteSolutionsContent() {
           <div className="container">
             <div className="complete-solutions__wrapper">
               <h2 className='display-2 mb-30 mt-25 neutral-0 complete-solutions__title'>
-                Services to Bring Your Vision to Life
+                {t("SolutionsBannerTitle")}
               </h2>
               <h3 className='text-lg neutral-500 mb-55 complete-solutions__descr'>
-                Tired of endless back-and-forth just to estimate your project? We’ve streamlined the process with ready-made packages tailored for your needs. Skip the hassle – explore, choose, and watch your idea take off!
+                {t("SolutionsBannerDescr")}
               </h3>
-              <Link className="btn banner-btn hover-up mr-5" href="https://calendly.com/maxopenstudio" >
+              <button
+                className="btn banner-btn hover-up mr-5"
+                onClick={() => openModal('calendar')}
+              >
                 <span>
                   <img src="/assets/imgs/template/icons/G.svg" alt="maxOpen" />
-                  Book a meeting
+                  {t("buttons.BookMeeting")}
                 </span>
-              </Link>
+              </button>
             </div>
           </div>
         </div>
@@ -40,54 +49,62 @@ function CompleteSolutionsContent() {
             <SolutionCard2 />
           </div>
         </div>
-      <div className='add-top-footer'>
-        <div className="container">
-          <div className='add-top-footer__wrapper'>
-            <div className='add-top-footer__text-block'>
-              <div className="solution-card__title text-wite">
-                Need help or have additional questions?
-              </div>
-              <div className='solution-card__descr'>
-                If you want to know more or have any questions about our work, we will be happy to help you! Book a call and we will discuss everything in detail.
-              </div>
-              <a
-                className="btn btn-brand-4-medium hover-up"
-                href="https://calendly.com/maxopenstudio" 
-              >
-                <span>
-                  Book a call
-                  <svg
-                    width="11"
-                    height="12"
-                    viewBox="0 0 11 12"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <g clipPath="url(#clip0_16_833)">
-                      <path
-                        d="M9.01605 3.72762L1.45505 11.2886L0.212891 10.0465L7.77301 2.48546H1.10981V0.728516H10.773V10.3917H9.01605V3.72762Z"
-                        fill="#191919"
-                      />
-                    </g>
-                    <defs>
-                      <clipPath id="clip0_16_833">
-                        <rect
-                          width="11"
-                          height="11"
-                          fill="white"
-                          transform="translate(0 0.5)"
+        <div className='add-top-footer'>
+          <div className="container">
+            <div className='add-top-footer__wrapper'>
+              <div className='add-top-footer__text-block'>
+                <div className="solution-card__title text-wite">
+                  {t("SolutionsNeedHelp")}
+                </div>
+                <div className='solution-card__descr'>
+                  {t("SolutionsHalpDescr")}
+                </div>
+                <a
+                  className="btn btn-brand-4-medium hover-up"
+                  onClick={() => openModal('calendar')}
+                >
+                  <span>
+                    {t("buttons.Book a call")}
+                    <svg
+                      width="11"
+                      height="12"
+                      viewBox="0 0 11 12"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <g clipPath="url(#clip0_16_833)">
+                        <path
+                          d="M9.01605 3.72762L1.45505 11.2886L0.212891 10.0465L7.77301 2.48546H1.10981V0.728516H10.773V10.3917H9.01605V3.72762Z"
+                          fill="#191919"
                         />
-                      </clipPath>
-                    </defs>
-                  </svg>
-                </span>
-              </a>
+                      </g>
+                      <defs>
+                        <clipPath id="clip0_16_833">
+                          <rect
+                            width="11"
+                            height="11"
+                            fill="white"
+                            transform="translate(0 0.5)"
+                          />
+                        </clipPath>
+                      </defs>
+                    </svg>
+                  </span>
+                </a>
+              </div>
+              <img src="/assets/imgs/template/top-footer-img.png" alt="image" />
             </div>
-            <img src="/assets/imgs/template/top-footer-img.png" alt="image" />
           </div>
         </div>
-      </div>
+
+        {/* ModalManager для відображення модалок */}
+        <ModalManager 
+          isOpen={isOpen} 
+          modalType={modalType} 
+          modalData={modalData} 
+          onClose={closeModal} 
+        />
       </Layout>
     </div>
-  )
+  );
 }
