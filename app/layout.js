36c3facecb4ff35b539690c204133409c9f44672
Kocherha Roman <f.your.d.2017@gmail.com>
2025-01-02 14:36:00 +0200

    added metadata

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,13 @@
+'use client';
 import { Urbanist } from 'next/font/google';
 import 'swiper/css';
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "/public/assets/css/style.css";
+import Head from 'next/head';
 import { ModalProvider } from '@/components/customHooks/useModal';
 import { LanguageProvider } from '@/components/customHooks/LanguageContext';
+import { useTranslation } from 'react-i18next';
 
 const urban = Urbanist({
     weight: ['200', '300', '400', '500', '600', '700'],
@@ -13,20 +16,29 @@ const urban = Urbanist({
     display: 'swap',
 });
 
-export const metadata = {
-    title: 'MaxOpen',
-};
+// export const metadata = {
+//     title: 'MaxOpen',
+// };
 
 export default function RootLayout({ children }) {
-    return (
-      <html lang="en">
-        <body className={urban.variable}>
-          <LanguageProvider>
-            <ModalProvider>
-              {children}
-            </ModalProvider>
-          </LanguageProvider>
-        </body>
-      </html>
-    );
+  const { t } = useTranslation();
+
+  return (
+    <html lang="en">
+      <Head>
+        <title>{t('metadata.MetaTitle')}</title>
+        <meta name="description" content={t('metadata.MetaDescription')} />
+        <meta name="keywords" content={t('metadata.MetaKeywords')} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="./favicon.ico" />
+      </Head>
+      <body className={urban.variable}>
+        <LanguageProvider>
+          <ModalProvider>
+            {children}
+          </ModalProvider>
+        </LanguageProvider>
+      </body>
+    </html>
+  );
 }
