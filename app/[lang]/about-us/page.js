ed6afaaff2775d7d6fa3315f3df5c8bfc0b4ad7f
Kocherha Roman <f.your.d.2017@gmail.com>
2025-01-07 13:03:03 +0200

    added about us page

--- app/[lang]/about-us/page.js
+++ app/[lang]/about-us/page.js
@@ -3,7 +3,6 @@ import React from "react";
 import Layout from '../layout';
 import { ModalProvider } from '@/components/customHooks/useModal';
 import { useTranslation } from 'react-i18next';
-import Loading from '@/components/elements/Loading';
 import PageBanner from '@/components/elements/PageBanner';
 
 export default function AboutUs() {
--- components/elements/Nav.jsx
+++ components/elements/Nav.jsx
@@ -10,10 +10,9 @@ export const Nav = () => {
       <ul className="main-menu">
         <li className="has-children"><Link className="active" href="/">{t("Home")}</Link></li>
         <li className="has-children"><Link href="#services">{t("Services")}</Link></li>
-        {/* <li className="has-children"><Link href="/about-us">{t("AboutUs")}</Link></li> */}
+        <li className="has-children"><Link href="/about-us">{t("AboutUs")}</Link></li>
         <li className="has-children"><Link href="/complete-solutions">{t("Solutions")}</Link></li>
-        <li className="has-children"><Link href="#projects">{t("Portfolio")}</Link>
-        </li>
+        <li className="has-children"><Link href="#projects">{t("Portfolio")}</Link></li>
         <li className="has-children"><Link href="#contact">{t("Contact")}</Link></li>
       </ul>
     </nav>
