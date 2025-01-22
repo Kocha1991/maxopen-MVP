4f36bce6c4830d0fd9846ecff7a05df447a4a79b
Kocherha Roman <f.your.d.2017@gmail.com>
2025-01-22 12:57:34 +0200

    fixed hrader

--- components/layout/header/CustomHeader.js
+++ components/layout/header/CustomHeader.js
@@ -38,43 +38,42 @@ export default function CustomHeader({ scroll, handleMobileMenu, topBar, headerC
                 </div>
             } */}
             <div className="container">
-                <div className="main-header custom-header">
-                    <div className="header-left">
-                        <div className="header-logo">
-                            <Link className="d-flex" href="/">
-                                <img 
-                                  src="/assets/imgs/template/logo.png" 
-                                  alt="MaxOpen"
-                                  className='web-logo'
-                                />
-                                <img 
-                                  src="/assets/imgs/template/mobile-logo.svg"
-                                  alt="MaxOpen"
-                                  className='mobile-logo'
-                                />
-                            </Link>
-                        </div>
-                        <div className="header-nav">
-                            <nav className="nav-main-menu d-none d-xl-block">
-                              <Nav />
-                            </nav>
-                        </div>
-                    </div>
-                    <div className="header-right ">
-                        <div className='header-right__block'>
-                          <LanguageDropdown />
-                          <button 
-                              className="btn btn-brand-4-medium hover-up"
-                              onClick={handleCalendarOpen}
-                          >
-                              <span>{t("buttons.Get started")}</span>
-                          </button>
-                        </div>
-                        <div className="burger-icon burger-icon-white burger-icon-custom" onClick={handleMobileMenu}>
-                            <img src="/assets/imgs/template/icons/burger.svg" alt="burger-menu" />
-                        </div>
-                    </div>
+              <div className="custom-header">
+                <div className="header-logo">
+                  <Link className="d-flex" href="/">
+                    <img 
+                      src="/assets/imgs/template/logo.png"
+                      alt="MaxOpen"
+                      className='web-logo'
+                    />
+                    <img 
+                      src="/assets/imgs/template/mobile-logo.svg"
+                      alt="MaxOpen"
+                      className='mobile-logo'
+                    />
+                  </Link>
                 </div>
+
+                <div className="header-nav">
+                  <nav className="nav-main-menu d-none d-xl-block">
+                    <Nav />
+                  </nav>
+                </div>
+                <div className="header-right ">
+                  <div className='header-right__block'>
+                    <LanguageDropdown />
+                    <button 
+                        className="btn btn-brand-4-medium hover-up"
+                        onClick={handleCalendarOpen}
+                    >
+                        <span>{t("buttons.Get started")}</span>
+                    </button>
+                  </div>
+                  <div className="burger-icon burger-icon-white burger-icon-custom" onClick={handleMobileMenu}>
+                      <img src="/assets/imgs/template/icons/burger.svg" alt="burger-menu" />
+                  </div>
+                </div>
+              </div>
             </div>
         </header>
     </>
